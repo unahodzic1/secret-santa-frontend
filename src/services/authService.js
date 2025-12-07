cf18993dfd400891d3dc1807884667af0c552301
unahodzic1 <uhodzic1@etf.unsa.ba>
2025-12-07 15:34:46 +0100

    env

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5015/api'; 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL; 
 
 const getCurrentUser = () => {
     const user = localStorage.getItem("user");
--- src/services/secretSantaService.js
+++ src/services/secretSantaService.js
@@ -1,6 +1,6 @@
 import authService from './authService';
 
-const API_BASE_URL = 'http://localhost:5015/api'; 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL; 
 
 const getAuthHeaders = () => {
     const user = authService.getCurrentUser();
