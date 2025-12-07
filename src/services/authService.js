6ee4d70f9a1702722fecd7b716601135f91b6f66
unahodzic1 <uhodzic1@etf.unsa.ba>
2025-12-07 15:43:10 +0100

    Url zamijenjen

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL; 
+const API_BASE_URL = 'https://secret-santa-backend-2hvs.onrender.com/api';
 
 const getCurrentUser = () => {
     const user = localStorage.getItem("user");
--- src/services/secretSantaService.js
+++ src/services/secretSantaService.js
@@ -1,6 +1,6 @@
 import authService from './authService';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL; 
+const API_BASE_URL = 'https://secret-santa-backend-2hvs.onrender.com/api';
 
 const getAuthHeaders = () => {
     const user = authService.getCurrentUser();
